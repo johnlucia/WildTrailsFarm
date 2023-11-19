2b0e2b8d379fb171a1327e1cf2c0b97b30b9eee2
John Lucia <johnmlucia@gmail.com>
2023-11-19 07:01:03 -0800

    trails showing up on map

---
+++ components/Trails.js
@@ -0,0 +1,28 @@
+import { useState }  from 'react';
+import { Polyline } from 'react-native-maps';
+
+export default function Trails({trails, onTapTrail, dashPattern, activeTrailID, visible}) {
+  if(!visible) {return(null);}
+  if(!trails) {return(null);}
+  let trailLines
+  const trailPolyLines = () => {
+    trailLines = trails.map((trail) => {
+      let trailActive = activeTrailID === trail.uid
+      return( <Polyline 
+                key={trail.id}
+                coordinates={trail.coordinates}
+                strokeColor={trail.color}
+                lineDashPattern={dashPattern}
+                lineJoin='round'
+                strokeWidth={trailActive ? 7:3}
+                tappable={true}
+                onPress={() => onTapTrail(trail)}
+              />
+            )
+    })
+
+    return(trailLines);
+  }
+
+  return(trailPolyLines());
+}
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Trails from '../components/Trails';
+
+
+
 
 export default function MapScreen() {
+
+  // liveTrailData is an array of trail objects
+  const [skiTrails, setSkiTrails] = useState(null);
+  const [activeTrailID, setActiveTrailID] = useState(0);
+  const [activeTrail, setActiveTrail] = useState(null);
+  const [trailInfoVisible, setTrailInfoVisible] = useState(false);
+
+  const onTapTrail = (trail) => {
+    setActiveTrailID(trail.uid);
+    setActiveTrail(trail);
+    setTrailInfoVisible(true);
+  }
+
+  const skiTrailData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('live-trail-data');
+      const trailArray = JSON.parse(jsonValue).trails;
+      setSkiTrails(trailArray);
+      return trailArray;
+    } catch (e) {
+      // error fetching data
+    }
+  };
+
+  useEffect(() => {
+    skiTrailData();
+  }, []);
+
   return (
     <View>
-      <Text>BOOM! Map Screen</Text>
+      <MapView 
+        style={styles.mapStyle}
+        initialRegion={{
+          latitude: 43.262,
+          longitude: -72.523,
+          latitudeDelta: 0.0522,
+          longitudeDelta: 0.0221
+        }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        showsPointsOfInterest={false}
+        showsScale={true}
+      >
+        {/* MAP FEATURES GO HERE */}
+        <Trails trails={skiTrails} onTapTrail={onTapTrail} activeTrailID={activeTrailID} visible={true} />
+
+
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
+});
